59bf2a578119c835d09540a55772339407465ed1
Muhammad Usman Shamas <47473401+Muhammad-Usman56@users.noreply.github.com>
2020-12-12 17:06:30 +0500

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -2,13 +2,7 @@ import firebase from 'firebase';
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDBI14Fpl0XiBue4-SJbMi7j5CG4OaB_dw",
-    authDomain: "crudapp-63c44.firebaseapp.com",
-    databaseURL: "https://crudapp-63c44.firebaseio.com",
-    projectId: "crudapp-63c44",
-    storageBucket: "crudapp-63c44.appspot.com",
-    messagingSenderId: "240063323473",
-    appId: "1:240063323473:web:aff1e29ee04032dbe0c770"
+    
   };
   // Initialize Firebase
 const fire =  firebase.initializeApp(firebaseConfig);
